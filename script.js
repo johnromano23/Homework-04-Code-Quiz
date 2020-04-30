239140d0b3cc55f9f1c810bf2e25e51e77106f37
John Romano <john_romano23@yahoo.com>
2020-04-29 22:36:50 -0400

    Submitting Web API Homework

---
+++ script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "alerts", "booleans", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if/else statement is enclosed within ___",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in Javascript can be used to store ___",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+    }, 
+    {
+      title: "String values must be enclosed within ___ when being assigned to variables",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    }, 
+    {
+      title: "A very useful tool used during development and debugging for printing conent to the debugger is?",
+      choices: ["console.log", "for loops", "terminal/bash", "javascript"],
+      answer: "console.log"
+    }, 
+    
+  ];
